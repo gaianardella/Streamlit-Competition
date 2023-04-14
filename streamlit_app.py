b64e8d39266a3d7234483109b8217cd589d32185
gaianardella <105795404+gaia-nardella@users.noreply.github.com>
2023-04-14 11:00:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,22 @@
-import streamlit
-import snowflake.connector
+import streamlit as st
+# import snowflake.connector
+
+# Define username and password
+CORRECT_USERNAME = "username"
+CORRECT_PASSWORD = "password"
+
+# Create a title and subheader
+st.title("Login Page")
+st.subheader("Enter your credentials to log in.")
+
+# # Create input fields for username and password
+# username = st.text_input("Username")
+# password = st.text_input("Password", type="password")
+
+# # Create a button to submit login information
+# if st.button("Login"):
+#     if username == CORRECT_USERNAME and password == CORRECT_PASSWORD:
+#         st.success("You have successfully logged in.")
+#     else:
+#         st.error("Invalid username or password.")
+
