0cbf54a13dbf9de0e07fd9e6dfda7cf80e9bd252
gaianardella <105795404+gaia-nardella@users.noreply.github.com>
2023-04-14 11:26:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import streamlit as st  # pip install streamlit
 
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title="A Cloud Closet", page_icon=":dress:", layout="wide")
 
 
 # # --- USER AUTHENTICATION ---
