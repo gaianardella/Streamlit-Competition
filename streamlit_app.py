d567ea5801fdfe96ed8557397d93d519cc554756
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-04-16 22:36:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
-# import pickle
-# from pathlib import Path
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
+from io import StringIO
+from PIL import Image
 
-# import pandas as pd  # pip install pandas openpyxl
-# import plotly.express as px  # pip install plotly-express
-import streamlit as st  # pip install streamlit
-# import streamlit_authenticator as stauth  # pip install streamlit-authenticator
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="A Cloud Closet", page_icon=":dress:", layout="wide")
 
+my_color_list=["Blue", "Red", "White", "Black", "Green", "Yellow", "Purple", "Pink"]
+my_item_list=["Sweater", "Trousers", "T-Shirt", "Shorts"]
+# my_color_list = my_color_list.set_index('Color')
 
 # Define the logout function
 def logout():
@@ -13,245 +17,175 @@ def logout():
     # For example, you can clear session data or redirect to a login page
     st.write("Logout clicked")
 
-# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="A Cloud Closet", page_icon=":dress:", layout="wide")
-
-
 # --- USER AUTHENTICATION ---
 
 # Define username and password
 CORRECT_USERNAME = "username"
 CORRECT_PASSWORD = "password"
 
-# Create a title and subheader
-st.title("Login Page")
-st.subheader("Enter your credentials to log in.")
-# st.header("Enter your credentials to log in.") #is bigger
+
+if 'login' not in st.session_state:
+   # Create a title and subheader
+    st.title("Login Page")
+    st.subheader("Enter your credentials to log in.")
+    login_form = st.form(key='login_form')
+    username = login_form.text_input(label='username')
+    password = login_form.text_input(label='password', type='password')
+    submit_button = login_form.form_submit_button(label='submit')
 
 
-# Create input fields for username and password
-username = st.text_input('Username')
-password = st.text_input('Password', type='password')
+    if submit_button:
+        if username == CORRECT_USERNAME and password == CORRECT_PASSWORD:
+            st.session_state.username = username
+            st.session_state['login'] = True
+            st.success("You have successfully logged in.")
+            # show_question = True
+            st.experimental_rerun()
+        else:
+            st.error('Invalid username or password')
+            # show_question = False
+    # else:
+    #     show_question = False
 
-if st.button("Login"):
-    if username == CORRECT_USERNAME and password == CORRECT_PASSWORD:
-        st.success("You have successfully logged in.")
-        show_question = True
-    else:
-        st.error('Invalid username or password')
-        show_question = False
-else:
-    show_question = False
+if 'login' in st.session_state:
+    st.empty()
+    # st.write(f"hello, {CORRECT_USERNAME}")
+    # if show_question:
+    # Display the main page
+    # st.title("Hello world!")
+    # st.write("Welcome to the main page.")
     
-# Define the format function
-def format_option(option):
-    return "• " + option
+    # Display the sidebar menu
+    with st.sidebar:
+        selected = option_menu("Main Menu", ["Home", "Upload Clothes", "Pick me an outfit", "Give me some stats", "Settings"], 
+            icons=['house', 'box-arrow-in-up', 'palette-fill', 'bar-chart-fill', 'gear'], menu_icon="cast", default_index=0)
 
-if show_question:
-    # ---- SIDEBAR ----
-    # Add widgets to the sidebar
-    st.sidebar.header("Sidebar")
-    if st.sidebar.button("Logout"):
-        logout()
-    
+    # Display the selected page
+    if selected == "Home":
+        st.title("Home page")
+        st.header("This is the Home page.")
+        st.subheader("1) Upload your photos")
+        st.write("Upload your clothes photos in the app")
+        st.subheader("2) Generate an outfit")
+        st.write("Click to generate an outfit")
+        st.subheader("3) Manage your wardrobe")
+        st.write("See which clothes you never wear")
+    elif selected == "Upload Clothes":
+        st.title("Upload your clothes")
+        st.subheader("This is the Upload Clothes page.")
+
+        # Let's put a pick list here so they can pick the fruit they want to include 
+        item_selected = st.multiselect("Pick item:", list(my_item_list), ['Sweater','Shorts'])
+        # colors_to_show = my_color_list[colors_selected] #pandas.dataFrame.loc[source] Access a group of rows and columns by label(s) or a boolean array.
+        # Display the table on the page.
+        # st.dataframe(colors_to_show)
         
-# Add other widgets to the sidebar here
-    st.sidebar.title(f"Welcome {CORRECT_USERNAME}")
-    st.sidebar.header("Please Filter Here:")
-    choice = st.sidebar.radio(
-        "Select choice:",
-        options=["Upload Clothes", "Pick me an outfit", "Give me some stats", "I need help"] #,
-#         format_func=format_option
-#         index=0
-    )
+        show_upload=False
+        if st.button("Submit Item"):
+            show_upload=True
     
-    if choice =="Upload Clothes":
-        st.subheader("Welcome to the Upload Clothes page")
-    elif choice =="Pick me an outfit":
-        st.subheader("Welcome to the Pick me an outfit page")
-    elif choice =="Give me some stats":
-        st.subheader("Welcome to the Give me some stats page")
-    else:
-        st.subheader("How to use the app")
+        if show_upload:
+            colors_selected = st.multiselect("What color is the item:", list(my_color_list), ['Blue','Red'])
+            #single file uploader (doesn't accept more than one file)
+            uploaded_file = st.file_uploader("Choose a file")
+            if uploaded_file is not None:
+                # To read file as bytes:
+                bytes_data = uploaded_file.getvalue()
+                st.write(bytes_data)
+                # To convert to a string based IO:
+                stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+                # st.write(stringio)
+                # # To read file as string:
+                # string_data = stringio.read()
+                # st.write(string_data)
+
+            # # Can be used wherever a "file-like" object is accepted:
+            # dataframe = pd.read_csv(uploaded_file)
+            # st.write(dataframe)
+
+    elif selected == "Pick me an outfit":
+        st.title("Generate an outfit")
+        st.subheader("This is the Pick me an outfit page.")
+        temperature = st.radio("What\'s the temperature?",('Hot', 'Cold'))
+
+        if temperature == 'Hot':
+            st.write('You selected hot.')
+        else:
+            st.write('You selected cold.')
+
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            st.header("Top")
+            top = Image.open('sweater.jpeg')
+            st.image(top, width=340)
+            
+
+        with col2:
+            st.header("Bottom")
+            bottom = Image.open('trousers.jpeg')
+            st.image(bottom, width=340)
+
+      
+        with col3:
+            var=-1
+            # if st.button("I don't like it", key="dislike"):
+            #     show_generate=True
+            # if st.button("I like it", key="like"):
+            #     show_generate=False
+
+            st.markdown("""
+                <style>
+                    div.stButton > button:first-child {
+                        text-align:center;
+                        background-color: #FF6347;
+                        padding-left: 20px;
+                        padding-right: 20px;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        height: 100%; /* set the height of the container to 100% */
+                    }
+                </style>
+            """, unsafe_allow_html=True)
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            if st.button("Disike"):
+                var = 0
+
+            st.markdown("""
+                <style>
+                    div.stButton > button:first-child {
+                        text-align:center;
+                        background-color: #00FF00;
+                        padding-left: 20px;
+                        padding-right: 20px;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        height: 100%; /* set the height of the container to 100% */
+                    }
+                </style>
+            """, unsafe_allow_html=True)
+            if st.button("Like"):
+                var = 1
         
-        
-# # --- USER AUTHENTICATION ---
-# names = ["Peter Parker", "Rebecca Miller"]
-# usernames = ["pparker", "rmiller"]
+        if var==1:
+            st.write("Cool!")
+        elif var == 0:
+            st.write("generate again")
+            
 
-# # load hashed passwords
-# file_path = Path(__file__).parent / "hashed_pw.pkl"
-# with file_path.open("rb") as file:
-#     hashed_passwords = pickle.load(file)
-
-# authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
-#     "sales_dashboard", "abcdef", cookie_expiry_days=30)
-
-# name, authentication_status, username = authenticator.login("Login", "main")
-
-# if authentication_status == False:
-#     st.error("Username/password is incorrect")
-
-# if authentication_status == None:
-#     st.warning("Please enter your username and password")
-
-# if authentication_status:
-#     # ---- READ EXCEL ----
-#     @st.cache
-#     def get_data_from_excel():
-#         df = pd.read_excel(
-#             io="supermarkt_sales.xlsx",
-#             engine="openpyxl",
-#             sheet_name="Sales",
-#             skiprows=3,
-#             usecols="B:R",
-#             nrows=1000,
-#         )
-#         # Add 'hour' column to dataframe
-#         df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
-#         return df
-
-#     df = get_data_from_excel()
-
-#     # ---- SIDEBAR ----
-#     authenticator.logout("Logout", "sidebar")
-#     st.sidebar.title(f"Welcome {name}")
-#     st.sidebar.header("Please Filter Here:")
-#     city = st.sidebar.multiselect(
-#         "Select the City:",
-#         options=df["City"].unique(),
-#         default=df["City"].unique()
-#     )
-
-#     customer_type = st.sidebar.multiselect(
-#         "Select the Customer Type:",
-#         options=df["Customer_type"].unique(),
-#         default=df["Customer_type"].unique(),
-#     )
-
-#     gender = st.sidebar.multiselect(
-#         "Select the Gender:",
-#         options=df["Gender"].unique(),
-#         default=df["Gender"].unique()
-#     )
-
-#     df_selection = df.query(
-#         "City == @city & Customer_type ==@customer_type & Gender == @gender"
-#     )
-
-#     # ---- MAINPAGE ----
-#     st.title(":bar_chart: Sales Dashboard")
-#     st.markdown("##")
-
-#     # TOP KPI's
-#     total_sales = int(df_selection["Total"].sum())
-#     average_rating = round(df_selection["Rating"].mean(), 1)
-#     star_rating = ":star:" * int(round(average_rating, 0))
-#     average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
-
-#     left_column, middle_column, right_column = st.columns(3)
-#     with left_column:
-#         st.subheader("Total Sales:")
-#         st.subheader(f"US $ {total_sales:,}")
-#     with middle_column:
-#         st.subheader("Average Rating:")
-#         st.subheader(f"{average_rating} {star_rating}")
-#     with right_column:
-#         st.subheader("Average Sales Per Transaction:")
-#         st.subheader(f"US $ {average_sale_by_transaction}")
-
-#     st.markdown("""---""")
-
-#     # SALES BY PRODUCT LINE [BAR CHART]
-#     sales_by_product_line = (
-#         df_selection.groupby(by=["Product line"]).sum()[["Total"]].sort_values(by="Total")
-#     )
-#     fig_product_sales = px.bar(
-#         sales_by_product_line,
-#         x="Total",
-#         y=sales_by_product_line.index,
-#         orientation="h",
-#         title="<b>Sales by Product Line</b>",
-#         color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
-#         template="plotly_white",
-#     )
-#     fig_product_sales.update_layout(
-#         plot_bgcolor="rgba(0,0,0,0)",
-#         xaxis=(dict(showgrid=False))
-#     )
-
-#     # SALES BY HOUR [BAR CHART]
-#     sales_by_hour = df_selection.groupby(by=["hour"]).sum()[["Total"]]
-#     fig_hourly_sales = px.bar(
-#         sales_by_hour,
-#         x=sales_by_hour.index,
-#         y="Total",
-#         title="<b>Sales by hour</b>",
-#         color_discrete_sequence=["#0083B8"] * len(sales_by_hour),
-#         template="plotly_white",
-#     )
-#     fig_hourly_sales.update_layout(
-#         xaxis=dict(tickmode="linear"),
-#         plot_bgcolor="rgba(0,0,0,0)",
-#         yaxis=(dict(showgrid=False)),
-#     )
-
-
-#     left_column, right_column = st.columns(2)
-#     left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
-#     right_column.plotly_chart(fig_product_sales, use_container_width=True)
-
-
-#     # ---- HIDE STREAMLIT STYLE ----
-#     hide_st_style = """
-#                 <style>
-#                 #MainMenu {visibility: hidden;}
-#                 footer {visibility: hidden;}
-#                 header {visibility: hidden;}
-#                 </style>
-#                 """
-#     st.markdown(hide_st_style, unsafe_allow_html=True)
-    
-    
-    
-    
-    
-    
-#     ###############################
-#     # # import snowflake.connector
-
-# # # Define username and password
-# # CORRECT_USERNAME = "username"
-# # CORRECT_PASSWORD = "password"
-
-# # # Create a title and subheader
-# # st.title("Login Page")
-# # st.subheader("Enter your credentials to log in.")
-# # # st.header("Enter your credentials to log in.") #is bigger
-
-
-# # # Create input fields for username and password
-# # username = st.text_input('Username')
-# # password = st.text_input('Password', type='password')
-
-# # if st.button("Login"):
-# #     if username == CORRECT_USERNAME and password == CORRECT_PASSWORD:
-# #         st.success("You have successfully logged in.")
-# #         show_question = True
-# #     else:
-# #         st.error('Invalid username or password')
-# #         show_question = False
-# # else:
-# #     show_question = False
-
-# # # Show Question
-# # if show_question:
-# #     question_placeholder = st.empty()
-# #     options = ['Option A', 'Option B', 'Option C']
-# #     selected_option = st.selectbox('Select an option', options)
-
-# # #     if question_placeholder.button('Refresh'):
-# # #         selected_option = None
-
-# #     if selected_option:
-# #         question_placeholder.write('You selected:', selected_option)
+     
+    elif selected == "Give me some stats":
+        st.title("Stats")
+        st.subheader("This is the Give me some stats page.")
+    elif selected == "Settings":
+        st.title("Settings")
+        st.subheader("This is the Settings page.")
