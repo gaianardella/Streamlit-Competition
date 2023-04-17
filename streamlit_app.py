397b7ecaeeb88cedf4b697303e7b751276267599
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-04-17 17:57:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -118,7 +118,7 @@ if 'login' in st.session_state:
             bytes_data = uploaded_file.getvalue()
 #             st.write(bytes_data)
             # To convert to a string based IO:
-            stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+#             stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
             # st.write(stringio)
             # # To read file as string:
             # string_data = stringio.read()
