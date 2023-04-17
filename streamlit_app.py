f2f204580576e4f8ac4333b6bfe0c69b480d1915
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-04-17 11:57:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,22 +92,24 @@ if 'login' in st.session_state:
             if st.button("Submit Item"):
                 st.write("You selected :" + item_selected[0])
                 first_flag=True
+                
+                colors_selected = st.multiselect("What color is the item:", list(my_color_list), ['Blue','Red'])
+                if st.button("Submit Color"):
+            
+                    if len(colors_selected)>1:
+                        # Join the colors with commas, except for the last on
+                        colors_string = ', '.join(colors_selected[:-1])
+                        # Add the last color to the string
+                        colors_string += ' and ' + colors_selected[-1]
+                    else:
+                        colors_string = colors_selected[0]
+                
+                    st.write("You selected: "+ colors_string)
+                    second_flag = True
         else:
             st.error("Select only one item")
-                         
-        colors_selected = st.multiselect("What color is the item:", list(my_color_list), ['Blue','Red'])
-        if st.button("Submit Color"):
-            
-            if len(colors_selected)>1:
-                # Join the colors with commas, except for the last on
-                colors_string = ', '.join(colors_selected[:-1])
-                # Add the last color to the string
-                colors_string += ' and ' + colors_selected[-1]
-            else:
-                colors_string = colors_selected[0]
-                
-            st.write("You selected: "+ colors_string)
-            second_flag = True
+                                
+        
         
             
         
