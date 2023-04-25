b23a7f3a5d7fd8491c05cddd388c207cc3c316de
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-04-25 23:33:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -150,17 +150,17 @@ if 'login' in st.session_state:
                     
                     # Prepare a SQL query to insert the photo data and colors into the appropriate table
                     # Use a dynamic SQL query to generate the appropriate number of columns based on the length of the colors_selected list
-                    query = "INSERT INTO clothes_table (item_column, type"
-                    for i in range(len(colors_selected)):
-                        query += f", color{i+1}"
-                    query += ") VALUES ('{bytes_data}', '{item_selected}'"
-                    for color in colors_selected:
-                        query += f", '{color}'"
-                    query += ")"
+#                     query = "INSERT INTO clothes_table (item_column, type"
+#                     for i in range(len(colors_selected)):
+#                         query += f", color{i+1}"
+#                     query += ") VALUES ('{bytes_data}', '{item_selected}'"
+#                     for color in colors_selected:
+#                         query += f", '{color}'"
+#                     query += ")"
 #                     cnx.cursor().execute(query)
 
                     # Execute the SQL query using the established connection and the photo data
-                    cnx.cursor().execute(query)
+#                     cnx.cursor().execute(query)
 
                     # Close the database connection
                     cnx.close()
