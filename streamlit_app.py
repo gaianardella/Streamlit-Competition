0cccf18ad7b0784b6ae9d830d64792dfe56af1c2
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-04-27 00:24:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -128,7 +128,7 @@ if 'login' in st.session_state:
 #             string_data = bytes_data.decode('UTF-8')
             
             
-            sql=base64.b64encode(requests.get(uploaded_file).content).decode('ascii')
+            sql=base64.b64encode(uploaded_file.content).decode('ascii')
             st.write(sql)
 #             try:        
 #                 encoding = 'gb18030'
