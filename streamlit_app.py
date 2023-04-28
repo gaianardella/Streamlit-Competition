793c86da0cb3aacfafc679aa325c5e809b2f655a
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-04-29 01:42:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -196,7 +196,7 @@ if 'login' in st.session_state:
                         my_cur.execute("insert into clothes_table values ('" +"1"+ "', '" +bytes_data_in_hex+ "', '" +str(item_selected[0])+ "')")
 #                         my_cur.execute("insert into clothes_table values ('" +id+ "', '" +bytes_data+ "', '" +item_selected+ "')")
                         result = my_cur.fetchone()
-                        bytes_data = result[0]
+                        bytes_data = result[1]
                         st.write(bytes_data)
 #                         
                                   
