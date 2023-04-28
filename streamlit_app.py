e5a497db7003f60b8b72072852644f0498f11c13
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-04-29 01:45:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -195,9 +195,9 @@ if 'login' in st.session_state:
                     with cnx.cursor() as my_cur:
                         my_cur.execute("insert into clothes_table values ('" +"1"+ "', '" +bytes_data_in_hex+ "', '" +str(item_selected[0])+ "')")
 #                         my_cur.execute("insert into clothes_table values ('" +id+ "', '" +bytes_data+ "', '" +item_selected+ "')")
-                        result = my_cur.fetchall()
-#                         bytes_data = result[1]
-                        st.write(result)
+                        result = cur.fetchall()
+                        items = [row[0] for row in result]
+                        st.write(items)
 #                         
                                   
                         
