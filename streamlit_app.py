c77f61fce4b46f8526c01bdda63600d5f78247de
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-04-29 02:05:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -194,7 +194,7 @@ if 'login' in st.session_state:
                         my_cur.execute("insert into clothes_table values ('" +id+ "', '" +bytes_data_in_hex+ "', '" +str(item_selected[0])+ "')")
 #                         my_cur.execute("insert into clothes_table values ('" +id+ "', '" +bytes_data+ "', '" +item_selected+ "')")
 #                         my_cur.execute("SELECT * FROM clothes_table WHERE id = %s", (id,))
-                        my_cur.execute("SELECT * FROM clothes_table WHERE id = 'e942b504-aa7f-430e-b350-724128ca0a44'")
+                        my_cur.execute("SELECT * FROM clothes_table)
                         byte_array=my_cur.fetchall()
                         st.write(byte_array)
                         st.stop()
