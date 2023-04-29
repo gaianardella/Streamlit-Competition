6b91e1af2de0da20736fe26887a7fcbf72aa3df1
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-04-29 02:30:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -191,8 +191,6 @@ if 'login' in st.session_state:
 #                     uuid.uuid4().hex
 #                     id=id.hex
                     with cnx.cursor() as my_cur:
-                        st.write(str(colors_selected))
-                        st.stop()
                         my_cur.execute("insert into clothes_table values ('" +id+ "', '" +bytes_data_in_hex+ "', '" +str(item_selected[0])+ "', '" +str(colors_selected)+"')")
 #                         my_cur.execute("insert into clothes_table values ('" +id+ "', '" +bytes_data+ "', '" +item_selected+ "')")
 #                         my_cur.execute("SELECT * FROM clothes_table WHERE id = %s", (id,))
